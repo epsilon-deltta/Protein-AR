bcfd86aab4c9a8e67b463306898a9ffe529563e0
epsilon-deltta <kokomong1316@gmail.com>
2021-12-02 12:03:21 +0900

    modulation of dataset&modelLoading

--- train.py
+++ train.py
@@ -13,31 +13,11 @@ args = parser.parse_args()
 # model load and settings
 from models import *
 
-model = args.model
+model = args.model.lower()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
-model = model.lower()
-if model.startswith('maxfil'):
-    model = MaxFilterCNN().to(device)
-elif model.startswith('resnet'):
-    model = ResNet().to(device)
-elif model.startswith('resnext'):
-    model = ResNext().to(device)
-elif model.startswith('attn'):
-    if model == 'attn':
-        transform = 'onehot'
-        model = attns().to(device)
-    elif model == 'attn0':
-        transform = None
-        model = attns0().to(device)
-elif model.startswith('lstm'):
-    if model == 'lstm':
-        model = lstm().to(device)
-        transform = 'onehot'
-    elif model == 'lstm0':
-        model = lstm0().to(device)
-        transform = None
+model,transfrom = get_model(model)
+model = model.to(device)
 
 loss = nn.CrossEntropyLoss()
 params = [p for p in model.parameters() if p.requires_grad]
@@ -45,64 +25,7 @@ opt  = torch.optim.Adam(params)
 
 # dataset and loader
 
-def get_vocab_map(vocab_path='./data/vocab.txt'):
-    with open(vocab_path,'r') as f:
-        vocab = f.read()
-
-    vocab = vocab.replace('\n','')
-
-    import re
-    p = re.compile('\s+')
-    vocab = re.sub(p,' ',vocab)
-
-    import ast
-    vocab = ast.literal_eval(vocab.split('=')[1].strip())
-
-    len_vocab = len(vocab)
-    vocab_map = dict(zip(vocab,range(len_vocab)))
-    return vocab_map
-vocab_map =get_vocab_map()
-
-import torch
-from torch.nn import functional as F
-import pandas as pd
-class ProteinDataset(torch.utils.data.Dataset):
-    def __init__(self,path,transform='oneHot'):
-        self.df        = pd.read_csv(path)
-        if type(transform) == str:
-            self.transform = transform.lower()
-        else:
-            self.transform = transform
-        
-    def __getitem__(self,idx):
-        item = self.df.iloc[idx]
-        x = item['seq']
-        y = item['label']
-        
-        if self.transform == 'onehot':
-            x = self.seq2oneHot(x)
-        elif self.transform is None:
-            x = self.seq2int(x)
-            
-        y = torch.tensor(y)
-        # y = self.label2oneHot(y)
-        return x,y
-    
-    def __len__(self):
-        return len(self.df)
-    
-    def seq2int(self,seq):
-        seq2int = [ vocab_map[x] for x in list(seq) ]
-        seq2int = torch.tensor(seq2int)
-        return seq2int
-    
-    def seq2oneHot(self,seq): # 'ABCDE..' -> OneHot (10,20) 
-        seq2int = self.seq2int(seq)
-        oneHot = F.one_hot(seq2int,num_classes=len(vocab_map) )
-        return oneHot
-    
-    def label2oneHot(self,label):
-        return F.one_hot(torch.tensor(label),num_classes=2)
+from dataset import ProteinDataset
     
 trdt  = ProteinDataset('./data/split/train.csv',transform=transform)
 valdt = ProteinDataset('./data/split/val.csv'  ,transform=transform)
